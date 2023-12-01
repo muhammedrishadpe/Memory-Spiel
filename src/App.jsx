dd20bc41ff0cc56d41e9c97ad938b6c51adbb196
muhammedrishadpe <rishadpe@gmail.com>
2023-12-01 21:02:00 +0100

    update Game Logic (When the user clicks the flip card, if it's false, it should become true, and it will be flipped)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const gameIcons = [
@@ -14,25 +15,62 @@ const gameIcons = [
   "🪞",
   "🔥",
   "🧨",
-  "🎇"
+  "🎇",
 ];
 
 function App() {
+  const [pieces, setPices] = useState([]);
+
+  const startGame = () => {
+    const dublicateGameIcons = gameIcons.concat(gameIcons);
+    const newGameIcons = [];
+    while (newGameIcons.length < gameIcons.length * 2) {
+      const randomIndex = Math.floor(Math.random() * dublicateGameIcons.length);
+      newGameIcons.push({
+        emoji: dublicateGameIcons[randomIndex],
+        flipped: false,
+        solved: false,
+        position: newGameIcons.length,
+      });
+      dublicateGameIcons.splice(randomIndex, 1);
+    }
+
+    setPices(newGameIcons);
+  };
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  const handleActive = (data) => {
+    const newPieces = pieces.map((piece) => {
+      if (piece.position === data.position) {
+        piece.flipped = !piece.flipped;
+      }
+      return piece;
+    });
+
+    setPices(newPieces);
+  };
+
+  console.log(pieces);
+
   return (
     <main>
       <h1>Memory Spiel</h1>
       <div className="container">
-        {gameIcons.map((data, index)=>(
-          <div className="flip-card" key={index}>
-          <div className="flip-card-inner">
-            <div className="flip-card-front" />
-            <div className="flip-card-back">
-            {data}
+        {pieces.map((data, index) => (
+          <div
+            className={`flip-card ${data.flipped ? "active" : ""}`}
+            key={index}
+            onClick={() => handleActive(data)}
+          >
+            <div className="flip-card-inner">
+              <div className="flip-card-front" />
+              <div className="flip-card-back">{data.emoji}</div>
             </div>
           </div>
-        </div>
         ))}
-        
       </div>
     </main>
   );
